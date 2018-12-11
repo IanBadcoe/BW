d7cb056d96f90e52fae49e4f3f06894da64f9136
Ian Badcoe <ibadcoe@Sumo-Digital.com>
2018-12-11 09:10:29 +0000

    moving over to BWBlank

---
+++ Source/BWBlank/BWBlankGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BWBlankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BWBLANK_API ABWBlankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
--- Source/BlockWorld3/BlockWorld3.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BlockWorld3.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BlockWorld3, "BlockWorld3" );
